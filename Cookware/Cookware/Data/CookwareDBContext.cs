9e6ea96e47c3c5aac43161867fb4543e36a8cbe8
Danul De Leon <danul.deleon@gmail.com>
2018-11-13 18:50:33 -0800

    local db working

---
+++ Cookware/Cookware/Data/CookwareDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Data
+{
+    public class CookwareDBContext : DbContext
+    {
+        public CookwareDBContext(DbContextOptions<CookwareDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Cookware/Cookware/Startup.cs
+++ Cookware/Cookware/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookware.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cookware
@@ -13,9 +16,21 @@ namespace Cookware
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<CookwareDBContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"])
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,11 +43,7 @@ namespace Cookware
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
