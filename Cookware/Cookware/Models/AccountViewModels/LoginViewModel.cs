5d45d2658f5c3726f05bff03e936bd5a835e4322
thejamilah <jamilahmcw@gmail.com>
2018-11-14 11:24:49 -0800

    Login and Register

---
+++ Cookware/Cookware/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public stiring Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
