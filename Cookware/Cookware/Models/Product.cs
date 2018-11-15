3df56831fa2fb9403e9b114eaf34753833207667
Danul De Leon <danul.deleon@gmail.com>
2018-11-14 20:01:48 -0800

    added product model

---
+++ Cookware/Cookware/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name="SKU Number")]
+        public string Sku { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Image { get; set; }
+
+    }
+}
