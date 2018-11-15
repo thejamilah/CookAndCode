f873d4229e95f480bd03e2a2f5ca6610079cd3e3
Danul De Leon <danul.deleon@gmail.com>
2018-11-14 20:43:58 -0800

    added functionality to controller

--- Cookware/Cookware/Controllers/ProductController.cs
+++ Cookware/Cookware/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Cookware.Models;
 using Cookware.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cookware.Controllers
 {
@@ -12,16 +13,32 @@ namespace Cookware.Controllers
     {
         private readonly IProducts _products;
 
-        //Index shows all products
-        public IActionResult Index()
+        public ProductController(IProducts products)
         {
-            return View();
+            _products = products;
+        }
+
+        //Index shows all products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _products.GetProducts());
         }
 
         //Details for one product
-        public IActionResult Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _products.GetProduct(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         //displays view with form
@@ -31,35 +48,92 @@ namespace Cookware.Controllers
         }
 
         //creates product after form submitted
-        public IActionResult CreateProduct()
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ID, Sku, Name, Price, Description, Image")] Product product)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                await _products.CreateProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
 
         //displays view with form to edit product
-        public IActionResult Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _products.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         //actually performs the action to edit
         [HttpPost]
-        public IActionResult Edit(int? id, Product product)
+        public async Task<IActionResult> Edit(int? id, [Bind("ID, Sku, Name, Price, Description, Image")] Product product)
         {
-            return View();
+            if (id != product.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _products.UpdateProduct(product);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
 
         //displays view with form to delete
-        public IActionResult DeleteProduct(int? id)
+        public async Task<IActionResult> DeleteProduct(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _products.GetProduct(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         //actually deletes product from database
         [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteProduct(int id)
+        public async Task<IActionResult> DeleteProduct(int id)
         {
-            return View();
+            await _products.DeleteProduct(id);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _products.GetProduct(id) != null;
         }
     }
 }
