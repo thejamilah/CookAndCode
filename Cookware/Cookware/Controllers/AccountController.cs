d7e45e78a2a217475ef501ccd043217338360172
Danul De Leon <danul.deleon@gmail.com>
2018-11-15 21:44:23 -0800

    added link to log and reg pages from homepage

--- Cookware/Cookware/Controllers/AccountController.cs
+++ Cookware/Cookware/Controllers/AccountController.cs
@@ -72,6 +72,12 @@ namespace Cookware.Controllers
             return View(registervm);
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginvm)
         {
