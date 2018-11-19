46b12b6b0251714db7e15e58c8ba1266f52a3f3e
thejamilah <jamilahmcw@gmail.com>
2018-11-18 16:42:11 -0800

    Resitstration code, migration, and update database

--- Cookware/Cookware/Controllers/AccountController.cs
+++ Cookware/Cookware/Controllers/AccountController.cs
@@ -10,19 +10,16 @@ using System;
 
 namespace Cookware.Controllers
 {
-    [AllowAnonymous]
-    public class AccountController : Controller 
+    public class AccountController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
         [HttpGet]
         public IActionResult Register()
         {
@@ -34,6 +31,7 @@ namespace Cookware.Controllers
         {
             if (ModelState.IsValid)
             {
+                // start the registration process
                 ApplicationUser user = new ApplicationUser()
                 {
                     UserName = registervm.Email,
@@ -44,60 +42,14 @@ namespace Cookware.Controllers
                 };
 
                 var result = await _userManager.CreateAsync(user, registervm.Password);
+
                 if (result.Succeeded)
                 {
-                    Claim fullNameClaim = new Claim("FullName", $"{user.FirstName} {user.LastName}");
-
-                    List<Claim> myclaims = new List<Claim>()
-                    {
-                        fullNameClaim
-                    };
-
-                    await _userManager.AddClaimAsync(user, myclaims[0]);
-
                     await _signInManager.SignInAsync(user, isPersistent: false);
-
-                    return RedirectToAction("Index", "Home");
                 }
 
-                else
-                {
-                    foreach (var item in result.Errors)
-                    {
-                        ModelState.AddModelError(" ", item.Description);
-                    }
-                }
             }
-
-            return View(registervm);
-        }
-
-        [HttpGet]
-        public IActionResult Login()
-        {
             return View();
         }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel loginvm)
-        {
-            if(ModelState.IsValid)
-            {
-                var result = await _signInManager.PasswordSignInAsync(loginvm.Email, loginvm.Password, false, false);
-
-                if(result.Succeeded)
-                {
-                    RedirectToAction("Index", "Home");
-                }
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Are you sure you entered the right info?");
-            }
-
-        return View(loginvm);
-
-        }
-
     }
 }
