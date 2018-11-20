2f6709ce9cab06850d677efac6bb44844517c909
Danul De Leon <danul.deleon@gmail.com>
2018-11-20 13:43:18 -0800

    Create basket item action created

---
+++ Cookware/Cookware/Models/Services/BasketItemService.cs
@@ -0,0 +1,49 @@
+﻿using Cookware.Data;
+using Cookware.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Models.Services
+{
+    public class BasketItemService : IBasketItem
+    {
+        private CookwareDBContext _context;
+
+        public BasketItemService(CookwareDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateBasketItem(BasketItem basketItem)
+        {
+            _context.BasketItems.Add(basketItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBasketItem(int id)
+        {
+            BasketItem basketItem = await GetBasketItem(id);
+            _context.BasketItems.Remove(basketItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<BasketItem> GetBasketItem(int? id)
+        {
+            return await _context.BasketItems.FirstOrDefaultAsync(x => x.ID == id);
+        }
+
+        public async Task<IEnumerable<BasketItem>> GetBasketItems()
+        {
+            return await _context.BasketItems.ToListAsync();
+        }
+
+        public async Task UpdateBasketItem(BasketItem basketItem)
+        {
+            _context.BasketItems.Update(basketItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
