c1378db7b6158bb8443e2967a4a74018f5b450ae
Danul De Leon <danul.deleon@gmail.com>
2018-11-20 21:01:17 -0800

    added view component, not sure if working yet

---
+++ Cookware/Cookware/Components/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using Cookware.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Components
+{
+    public class ShoppingCart : ViewComponent
+    {
+        private CookwareDBContext _context;
+
+        public ShoppingCart(CookwareDBContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult InvokeAsync()
+        {
+            var shoppingCart = _context.BasketItems.ToList();
+
+            return View(shoppingCart);
+
+        }
+    }
+}
