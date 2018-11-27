be611ee16abc710b1c353d52416e6bfaf4ba6bcd
Danul De Leon <danul.deleon@gmail.com>
2018-11-27 13:30:52 -0800

    page made

--- Cookware/Cookware/Controllers/CheckoutController.cs
+++ Cookware/Cookware/Controllers/CheckoutController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookware.Data;
+using Cookware.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
@@ -10,9 +14,22 @@ namespace Cookware.Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index()
+        private UserManager<ApplicationUser> _userManager;
+        private CookwareDBContext _context;
+
+        public CheckoutController(UserManager<ApplicationUser> userManager, CookwareDBContext context)
         {
-            return View();
+            _userManager = userManager;
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Receipt()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var ID = user.Id;
+            var shoppingCart = await _context.BasketItems.Where(x => x.UserID == ID).Include(product => product.Product).ToListAsync();
+
+            return View(shoppingCart);
         }
     }
 }
