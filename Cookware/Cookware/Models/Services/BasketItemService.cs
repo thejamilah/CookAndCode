8fec2b85532414baa6efe3cc933806e99f04680a
Danul De Leon <danul.deleon@gmail.com>
2018-11-27 22:02:40 -0800

    added summary comments

--- Cookware/Cookware/Models/Services/BasketItemService.cs
+++ Cookware/Cookware/Models/Services/BasketItemService.cs
@@ -21,12 +21,23 @@ namespace Cookware.Models.Services
             _userManager = userManager;
         }
 
+        /// <summary>
+        /// Creates new basket item in db
+        /// </summary>
+        /// <param name="basketItem">New BasketItem object to be added</param>
+        /// <returns>ask for added item</returns>
         public async Task CreateBasketItem(BasketItem basketItem)
         {
             _context.BasketItems.Add(basketItem);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Delete item from db
+        /// </summary>
+        /// <param name="ProductID">Product id</param>
+        /// <param name="ID">User id</param>
+        /// <returns>task for deleted item</returns>
         public async Task DeleteBasketItem(int ProductID, string ID)
         {
             BasketItem basketItem = await GetBasketItem(ProductID, ID);
@@ -34,19 +45,32 @@ namespace Cookware.Models.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Retrieves basket item by id's
+        /// </summary>
+        /// <param name="ProductID">Product Id</param>
+        /// <param name="ID">User id</param>
+        /// <returns>Basket item requested</returns>
         public async Task<BasketItem> GetBasketItem(int ProductID, string ID)
         {
             return await _context.BasketItems
                 .FirstOrDefaultAsync(x => x.ProductID == ProductID && x.UserID == ID);
-
-           
         }
 
+        /// <summary>
+        /// Gets all basket items in db
+        /// </summary>
+        /// <returns>list of basket items</returns>
         public async Task<IEnumerable<BasketItem>> GetBasketItems()
         {
             return await _context.BasketItems.ToListAsync();
         }
 
+        /// <summary>
+        /// Updates basket item with object
+        /// </summary>
+        /// <param name="basketItem">modified basket item object</param>
+        /// <returns>task for updated item</returns>
         public async Task UpdateBasketItem(BasketItem basketItem)
         {
             _context.BasketItems.Update(basketItem);
--- Cookware/Cookware/Models/Services/ProductService.cs
+++ Cookware/Cookware/Models/Services/ProductService.cs
@@ -17,12 +17,22 @@ namespace Cookware.Models.Services
             _context = context;
         }
 
+        /// <summary>
+        /// Adds new product into DB
+        /// </summary>
+        /// <param name="product">new product to be added</param>
+        /// <returns>Task but really just adds to db</returns>
         public async Task CreateProduct(Product product)
         {
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Delete product from DB
+        /// </summary>
+        /// <param name="id">Id for product to delete</param>
+        /// <returns>Task for deleted product</returns>
         public async Task DeleteProduct(int id)
         {
             Product product = await GetProduct(id);
@@ -30,16 +40,30 @@ namespace Cookware.Models.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Retrieves product from DB
+        /// </summary>
+        /// <param name="id">Id for reference</param>
+        /// <returns>Product requested by id</returns>
         public async Task<Product> GetProduct(int? id)
         {
             return await _context.Products.FirstOrDefaultAsync(x => x.ID == id);
         }
 
+        /// <summary>
+        /// Get list of all products in DB
+        /// </summary>
+        /// <returns>list of products</returns>
         public async Task<IEnumerable<Product>> GetProducts()
         {
             return await _context.Products.ToListAsync();
         }
 
+        /// <summary>
+        /// Updates product in db
+        /// </summary>
+        /// <param name="product">Product with altered information</param>
+        /// <returns>task for updated product</returns>
         public async Task UpdateProduct(Product product)
         {
             _context.Products.Update(product);
