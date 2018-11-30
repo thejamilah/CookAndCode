5d55bd777e89781c2a424a0f739c865c7452eac7
Danul De Leon <danul.deleon@gmail.com>
2018-11-29 16:53:34 -0800

    checkout page working

---
+++ Cookware/Cookware/Models/Services/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Cookware.Data;
+using Cookware.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookware.Models.Services
+{
+    public class OrderService : IOrder
+    {
+        private CookwareDBContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public OrderService(CookwareDBContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task CreateOrder(Order Order)
+        {
+            _context.Orders.Add(Order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
