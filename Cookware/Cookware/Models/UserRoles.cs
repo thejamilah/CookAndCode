10f2127e0ae66d53bbe481c02184e4cdb5249392
thejamilah <jamilahmcw@gmail.com>
2018-12-03 09:58:05 -0800

    Admin CRUD Razor Pages

--- Cookware/Cookware/Models/UserRoles.cs
+++ Cookware/Cookware/Models/UserRoles.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cookware.Models
 {
-    public class UserRoles
+    /// <summary>
+    /// These roles are used to enact user policies
+    /// </summary>
+    public class UserRoles : IdentityUser 
     {
         public const string Member = "Member";
         public const string Admin = "Administrator";
