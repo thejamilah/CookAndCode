2465f1ba4af97e4f6b03e6bfb7c5afd38fe7e411
Danul De Leon <danul.deleon@gmail.com>
2018-12-03 11:44:28 -0800

    delete function working

--- Cookware/Cookware/Pages/Admin/DeleteProducts.cshtml.cs
+++ Cookware/Cookware/Pages/Admin/DeleteProducts.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookware.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,38 @@ namespace Cookware.Pages.Admin
 {
     public class DeleteProductsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IProducts _product;
 
+        public DeleteProductsModel(IProducts product)
+        {
+            _product = product;
+        }
+
+        [BindProperty]
+        public Cookware.Models.Product Product { get; set; }
+
+        public async Task<IActionResult> OnGet(int? ID)
+        {
+            if (ID == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            Product = await _product.GetProduct(ID);
+
+            if(Product == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(int ID)
+        {
+            await _product.DeleteProduct(ID);
+
+            return RedirectToPage("./Index");
         }
     }
 }
