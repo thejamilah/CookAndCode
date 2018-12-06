f75a3f336a0beeb3e7df3009cdd0f214454575c0
thejamilah <jamilahmcw@gmail.com>
2018-12-05 21:34:00 -0800

    Working on last 10 items on Admin Dashboard

--- Cookware/Cookware/Pages/Admin/RecentOrders.cshtml.cs
+++ Cookware/Cookware/Pages/Admin/RecentOrders.cshtml.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookware.Data;
+using Cookware.Models;
+using Cookware.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +13,20 @@ namespace Cookware.Pages.Admin
 {
     public class RecentOrdersModel : PageModel
     {
+        private UserManager<ApplicationUser> _userManager;
+        private CookwareDBContext _context;
+        private readonly IProducts _products;
+        private readonly IBasketItem _basketItem;
+
+        public RecentOrdersModel(UserManager<ApplicationUser> userManager, IProducts products, IBasketItem basketItem, CookwareDBContext context)
+        {
+            _userManager = userManager;
+            _products = products;
+            _basketItem = basketItem;
+            _context = context;
+        }
+
+
         public void OnGet()
         {
 
