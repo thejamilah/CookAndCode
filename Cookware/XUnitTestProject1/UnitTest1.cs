19406dc6c365c35cede69e4617ac1659731dd533
Danul De Leon <danul.deleon@gmail.com>
2018-12-09 16:59:44 -0800

    product CRUD passing

--- Cookware/XUnitTestProject1/UnitTest1.cs
+++ Cookware/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
+using Cookware.Data;
 using Cookware.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
 
@@ -40,6 +42,60 @@ namespace XUnitTestProject1
 
             //Assert
             Assert.Equal("Chocolate Printer", product.Name);
+        }
+
+
+        /// <summary>
+        /// test can create, read, update, and delete product in db
+        /// </summary>
+        [Fact]
+        public async void CrudProductInDB()
+        {
+            //Arrange
+            //setup our DB
+            //set values
+
+            DbContextOptions<CookwareDBContext> options =
+            new DbContextOptionsBuilder<CookwareDBContext>().UseInMemoryDatabase("DbCanSave").Options;
+
+            //Act
+            //creating a variable that "gets" the name
+            using (CookwareDBContext context = new CookwareDBContext(options))
+            {
+                Product product = new Product();
+                product.Name = "Test Product";
+                product.Description = "123 test";
+                product.Price = 123.45M;
+                product.Image = "someUrl";
+                product.LanguageIsCSharp = true;
+
+                ////Act
+
+                context.Products.Add(product);
+                context.SaveChanges();
+
+                var productName = await context.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
+
+                //Assert
+                Assert.Equal("Test Product", productName.Name);
+
+                //UPDATE
+                product.Name = "Update Product";
+                context.Products.Update(product);
+                context.SaveChanges();
+
+                var updatedHotel = await context.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
+
+                Assert.Equal("Update Product", updatedHotel.Name);
+
+                //DELETE
+                context.Products.Remove(product);
+                context.SaveChanges();
+
+                var deletedHotel = await context.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
+
+                Assert.True(deletedHotel == null);
+            }
         }
     }
 }
