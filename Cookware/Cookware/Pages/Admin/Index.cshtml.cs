f862570f91ce9d8630d1e3ba84e5bf9b129d6c4b
Danul De Leon <danul.deleon@gmail.com>
2018-12-10 21:27:33 -0800

    products displaying in admin page

--- Cookware/Cookware/Pages/Admin/Index.cshtml.cs
+++ Cookware/Cookware/Pages/Admin/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookware.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,14 +12,20 @@ namespace Cookware.Pages.Admin
     [Authorize("AdminAccess")]
     public class IndexModel : PageModel
     {
-        public IndexModel()
-        {
 
+        private readonly IProducts _product;
+
+        public IndexModel(IProducts product)
+        {
+            _product = product;
         }
 
-        public void OnGet()
-        {
+        public IEnumerable<Cookware.Models.Product> Products { get; set; }
 
+        public async Task<IActionResult> OnGet()
+        {
+            Products = await _product.GetProducts();
+            return Page();
         }
     }
 }
